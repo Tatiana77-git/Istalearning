2c13ceffe4e5ff66ae6ce75a45605902aec5d58a
Tatiana77-git <tanya.dor1@gmail.com>
2026-03-26 16:10:57 +0100

    UI improvements: hero banner, footer, ocons

--- frontend/vite_project/src/components/Footer.tsx
+++ frontend/vite_project/src/components/Footer.tsx
@@ -38,7 +38,7 @@ function Footer() {
 <div className="footer-legal">
  <p>
   Adresse : 38 rue Charles de Gaulle, 68370 Orbey (France)<br />
-  autoentrepreneur – traductrice-interprète auprès du Tribunal de Colmar depuis 2015<br />
+  autoentrepreneur – traductrice-interprète assermentée auprès du Tribunal de Colmar depuis 2015<br />
   adresse mail : istalearning@gmail.com | téléphone +33 6 52 77 89 22<br />
   website: www.istalearning.com<br />
   N° SIRET : 830 997 888 00029 | R.C.S. Colmar | Code APE : 8559B
@@ -56,11 +56,12 @@ function Footer() {
 
           <div className="social-icons">
             <a href="https://chat.whatsapp.com/EKy683NY5xEFms2h1WUxi0?mode=hq1tshi" target="_blank" rel="noopener noreferrer"><img src="/icons/whatsapp.png" alt="WhatsApp" /></a>
-            <a href="#" target="_blank" rel="noopener noreferrer"><img src="/icons/linkedin.png" alt="LinkedIn" /></a>
-            <a href="#" target="_blank" rel="noopener noreferrer"><img src="/icons/tiktok.png" alt="TikTok" /></a>
+            <a href="https://www.linkedin.com/in/katrin-yakubovskaya-75167a154/" target="_blank" rel="noopener noreferrer"><img src="/icons/linkedin.png" alt="LinkedIn" /></a>
+            <a href="https://www.tiktok.com/@studyinfranceyes" target="_blank" rel="noopener noreferrer"><img src="/icons/tiktok.png" alt="TikTok" /></a>
             <a href="https://www.instagram.com/studyinfrance.katrin?igsh=dDFvcTh1Ymllb3Zy&utm_source=qr" target="_blank" rel="noopener noreferrer"><img src="/icons/instagram.png" alt="Instagram" /></a>
             <a href="https://youtube.com/@istalearning.fr_ekaterinayakub?si=lzBVVZpBoaOAQNZAs"  target="_blank" rel="noopener noreferrer"><img src="/icons/youtube.png" alt="YouTube" /></a>
             <a href="https://t.me/studyinfranceKATRIN" target="_blank" rel="noopener noreferrer"><img src="/icons/telegram.png" alt="Telegram" /></a>
+            <a href="https://taplink.cc/studyinfrance.katrin" target="_blank" rel="noopener noreferrer"><img src="/icons/taplink.png" alt="Taplink" /></a>
            
           </div>
 
--- frontend/vite_project/src/components/HeroBanner.tsx
+++ frontend/vite_project/src/components/HeroBanner.tsx
@@ -11,6 +11,18 @@ import banner6 from "../assets/banner6.jpg";
 import banner7 from "../assets/banner7.jpg";
 import banner8 from "../assets/banner8.jpg";
 import banner9 from "../assets/banner9.jpg";
+import banner10 from "../assets/banner10.jpg";
+import banner11 from "../assets/banner11.jpg";
+import banner12 from "../assets/banner12.jpg";
+import banner13 from "../assets/banner13.jpg";
+import banner14 from "../assets/banner14.jpg";
+import banner15 from "../assets/banner15.jpg";
+import banner16 from "../assets/banner16.jpg";
+import banner17 from "../assets/banner17.jpg";
+import banner18 from "../assets/banner18.jpg";
+
+
+
 
 
 
@@ -25,6 +37,18 @@ const slides: { src: string; type: string }[] = [
   { src: banner7, type: "certificate" },
   { src: banner8, type: "photo" },
   { src: banner9, type: "certificate" },
+  { src: banner10, type: "photo" },
+  { src: banner11, type: "certificate" },
+  { src: banner12, type: "photo" },
+  { src: banner13, type: "certificate" },
+  { src: banner14, type: "photo" },
+  { src: banner15, type: "certificate" },
+  { src: banner16, type: "photo" },
+  { src: banner17, type: "certificate" },
+  { src: banner18, type: "photo" },
+
+
+
 
 ];
 
@@ -48,7 +72,7 @@ function HeroBanner() {
 
      
       <div className="hero-text">
-        <h1>Cross-Cultural Studies Learning Centre</h1>
+        <h1>Cross-Cultural Studies: Learning and Translating🌍Interpreting Centre ISTALERNING</h1>
         <p>
           Des tests de langues ONLINE rapides, fiables et parfaitement
           objectifs certifiés par les linguistes diplômés.
